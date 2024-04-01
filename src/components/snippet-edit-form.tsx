42d60d56a76a6e8f0f7a66a50768258d28613072
acelero-webdev <webdev@acelero.net>
2024-03-31 22:06:11 -0500

    update the snippet edit form to use state and the monico react editor

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,13 +1,31 @@
 'use client';
 
 import type { Snippet } from "@prisma/client";
+import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
     snippet: Snippet
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps ) {
+    const [code, setCode] = useState(snippet.code);
+
+
+    const handleEditorChange = (value: string = "") => {
+        setCode(value);
+    }
+
     return (
-        <div>Client component has snippet with title {snippet.title} </div>
+        <div> 
+            <Editor
+                height="40vh"
+                theme="vs-dark"
+                language="javascript"
+                defaultValue={code}
+                options={{ minimap: { enabled: false }}}
+                onChange={handleEditorChange}
+            />
+        </div>
     )
 }
